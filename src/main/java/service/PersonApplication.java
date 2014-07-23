1888e6f3c8e84782a3b2c4e532837d238cc29545
Dan Siwiec <daniel.siwiec@gmail.com>
2014-07-23 00:18:51 -0700

    Added CORS headers

--- src/main/java/service/PersonApplication.java
+++ src/main/java/service/PersonApplication.java
@@ -6,7 +6,12 @@ import io.dropwizard.Application;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import resources.PersonResource;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class PersonApplication extends Application<Configuration> {
 
@@ -23,5 +28,16 @@ public class PersonApplication extends Application<Configuration> {
     public void run(Configuration configuration, Environment environment) throws Exception {
         environment.jersey().register(new PersonResource());
         environment.jersey().property(ResourceConfig.PROPERTY_CONTAINER_RESPONSE_FILTERS, LinkFilter.class);
+
+        addCorsHeaders(environment);
+    }
+
+    private void addCorsHeaders(Environment environment) {
+        FilterRegistration.Dynamic filter = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        filter.setInitParameter("allowedOrigins", "*");    // allowed origins comma separated
+        filter.setInitParameter("allowedHeaders", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin");
+        filter.setInitParameter("allowedMethods", "GET,PUT,POST,DELETE,OPTIONS,HEAD");
+        filter.setInitParameter("allowCredentials", "true");
     }
 }
